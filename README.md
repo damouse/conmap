54b0354dc18bf96ba1f1470b796ede9f35db8b82
damouse <damouse007@gmail.com>
2016-07-10 17:26:57 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Conmap
+
+Conmap is a simple concurrent map. 
---
+++ map_test.go
@@ -0,0 +1,22 @@
+package core
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestConcurrentBindingMap(t *testing.T) {
+	Convey("Binding maps should accept bindings", t, func() {
+		So(nil, ShouldBeNil)
+		// b := NewConcurrentBindingMap()
+
+		// a := &boundEndpoint{NewID(), "", nil}
+		// i := NewID()
+
+		// b.Set(i, a)
+
+		// c, _ := b.Get(i)
+		// So(c.callback, ShouldEqual, a.callback)
+	})
+}
