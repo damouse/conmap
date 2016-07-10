10536eb55540ee36bc01996fef94b84b266081f3
damouse <damouse007@gmail.com>
2016-07-10 17:54:32 -0500

    leaving the iterator aside for now

--- map.go
+++ map.go
@@ -1,14 +1,20 @@
 package core
 
-import "sync"
+import (
+	"hash/fnv"
+	"sync"
+)
 
-var SHARD_COUNT = 10
+type ShardSet []*Shard
 
 // A sharded concurrent map with internal RW locks
-type ConcurrentMap []*ConcurrentMapShared
+type ConcurrentMap struct {
+	shards []*Shard
+	count  int // The number of shards in the map
+}
 
-// A "thread" safe string to anything map.
-type ConcurrentMapShared struct {
+// A goroutine safe string to anything map.
+type Shard struct {
 	items map[uint64]interface{}
 	sync.RWMutex
 }
@@ -18,34 +24,34 @@ type Tuple struct {
 	Val interface{}
 }
 
-// Map of type [uint64: interface{}]
-func NewConcurrentMap() ConcurrentMap {
-	m := make(ConcurrentMap, SHARD_COUNT)
-	for i := 0; i < SHARD_COUNT; i++ {
-		m[i] = &ConcurrentMapShared{items: make(map[uint64]interface{})}
+func NewConcurrentMap(numShards int) ConcurrentMap {
+	m := ConcurrentMap{
+		shards: make([]*Shard, numShards),
+		count:  numShards,
 	}
+
+	for i := 0; i < numShards; i++ {
+		m.shards[i] = &Shard{items: make(map[uint64]interface{})}
+	}
+
 	return m
 }
 
-// Returns shard under given key
-func (m ConcurrentMap) GetShard(key uint64) *ConcurrentMapShared {
-	k := m[key%uint64(SHARD_COUNT)]
+func (m ConcurrentMap) GetShard(key uint64) *Shard {
+	k := m.shards[key%uint64(m.count)]
 
 	return k
 }
 
-// Sets the given value under the specified key.
 func (m *ConcurrentMap) Set(key uint64, value interface{}) {
-	// Get map shard.
 	shard := m.GetShard(key)
 	shard.Lock()
 	defer shard.Unlock()
+
 	shard.items[key] = value
 }
 
-// Retrieves an element from map under given key.
 func (m ConcurrentMap) Get(key uint64) (interface{}, bool) {
-	// Get shard
 	shard := m.GetShard(key)
 	shard.RLock()
 	defer shard.RUnlock()
@@ -55,19 +61,19 @@ func (m ConcurrentMap) Get(key uint64) (interface{}, bool) {
 	return val, ok
 }
 
-// Returns the number of elements within the map.
 func (m ConcurrentMap) Count() int {
 	count := 0
-	for i := 0; i < SHARD_COUNT; i++ {
-		shard := m[i]
+
+	for i := 0; i < m.count; i++ {
+		shard := m.shards[i]
 		shard.RLock()
 		count += len(shard.items)
 		shard.RUnlock()
 	}
+
 	return count
 }
 
-// Looks up an item under specified key
 func (m *ConcurrentMap) Has(key uint64) bool {
 	shard := m.GetShard(key)
 	shard.RLock()
@@ -77,12 +83,45 @@ func (m *ConcurrentMap) Has(key uint64) bool {
 	return ok
 }
 
-// Removes an element from the map.
 func (m *ConcurrentMap) RemoveKey(key uint64) {
 	shard := m.GetShard(key)
 	shard.Lock()
 	defer shard.Unlock()
 	delete(shard.items, key)
+}
+
+func (m *ConcurrentMap) IsEmpty() bool {
+	return m.Count() == 0
+}
+
+// Returns a buffered iterator which could be used in a for range loop.
+func (m ConcurrentMap) Iter() <-chan Tuple {
+	ch := make(chan Tuple, 10)
+
+	go func() {
+		for _, shard := range m.shards {
+			shard.RLock()
+
+			for key, val := range shard.items {
+				ch <- Tuple{key, val}
+			}
+
+			shard.RUnlock()
+		}
+		close(ch)
+	}()
+
+	return ch
+}
+
+func (m ConcurrentMap) Items() map[uint64]interface{} {
+	tmp := make(map[uint64]interface{})
+
+	for item := range m.Iter() {
+		tmp[item.Key] = item.Val
+	}
+
+	return tmp
 }
 
 // Remove an element by value and return it
@@ -94,113 +133,9 @@ func (m *ConcurrentMap) RemoveKey(key uint64) {
 //  delete(shard.items, key)
 // }
 
-// Checks if map is empty.
-func (m *ConcurrentMap) IsEmpty() bool {
-	return m.Count() == 0
+// Maps that want to use strings as keys should call this function when resolving keys
+func KeyFromString(k string) uint64 {
+	h := fnv.New64()
+	h.Write([]byte(k))
+	return h.Sum64()
 }
-
-// Returns a buffered iterator which could be used in a for range loop.
-func (m ConcurrentMap) Iter() <-chan Tuple {
-	ch := make(chan Tuple, 10)
-	go func() {
-		// Foreach shard.
-		for _, shard := range m {
-			// Foreach key, value pair.
-			shard.RLock()
-			for key, val := range shard.items {
-				ch <- Tuple{key, val}
-			}
-			shard.RUnlock()
-		}
-		close(ch)
-	}()
-	return ch
-}
-
-func (m ConcurrentMap) Items() map[uint64]interface{} {
-	tmp := make(map[uint64]interface{})
-
-	// Insert items to temporary map.
-	for item := range m.Iter() {
-		tmp[item.Key] = item.Val
-	}
-
-	return tmp
-}
-
-// // Specialized forms of the concurrent map
-// type BindingConcurrentMap struct {
-// 	ConcurrentMap
-// }
-
-// type BindingTuple struct {
-// 	Key uint64
-// 	Val *boundEndpoint
-// }
-
-// // Map of type [uint64: *boundEndpint]
-// func NewConcurrentBindingMap() BindingConcurrentMap {
-// 	return BindingConcurrentMap{NewConcurrentMap()}
-// }
-
-// func (m BindingConcurrentMap) Iter() <-chan BindingTuple {
-// 	ch := make(chan BindingTuple, 10)
-// 	go func() {
-// 		for _, shard := range m.ConcurrentMap {
-// 			shard.RLock()
-
-// 			for key, val := range shard.items {
-// 				ch <- BindingTuple{key, val.(*boundEndpoint)}
-// 			}
-
-// 			shard.RUnlock()
-// 		}
-
-// 		close(ch)
-// 	}()
-// 	return ch
-// }
-
-// // Sets the given value under the specified key.
-// func (m *BindingConcurrentMap) Set(key uint64, value *boundEndpoint) {
-// 	// Get map shard.
-// 	shard := m.GetShard(key)
-// 	shard.Lock()
-// 	defer shard.Unlock()
-// 	shard.items[key] = value
-// }
-
-// // Retrieves an element from map under given key.
-// func (m BindingConcurrentMap) Get(key uint64) (*boundEndpoint, bool) {
-// 	// Get shard
-// 	shard := m.GetShard(key)
-// 	shard.RLock()
-// 	defer shard.RUnlock()
-
-// 	// Get item from shard.
-// 	if val, ok := shard.items[key]; ok {
-// 		return val.(*boundEndpoint), true
-// 	} else {
-// 		return nil, false
-// 	}
-// }
-
-// // Get the given binding given its endpoint. Returns its key, binding, and found
-// func (m BindingConcurrentMap) GetWithEndpoint(endpoint string) (uint64, *boundEndpoint, bool) {
-// 	for _, shard := range m.ConcurrentMap {
-// 		shard.RLock()
-
-// 		for key, val := range shard.items {
-// 			v := val.(*boundEndpoint)
-
-// 			if v.endpoint == endpoint {
-// 				shard.RUnlock()
-// 				return key, v, true
-// 			}
-// 		}
-
-// 		shard.RUnlock()
-// 	}
-
-// 	return 0, nil, false
-// }
--- map_test.go
+++ map_test.go
@@ -3,20 +3,77 @@ package core
 import (
 	"testing"
 
-	. "github.com/smartystreets/goconvey/convey"
+	. "github.com/stretchr/testify/assert"
 )
 
-func TestConcurrentBindingMap(t *testing.T) {
-	Convey("Binding maps should accept bindings", t, func() {
-		So(nil, ShouldBeNil)
-		// b := NewConcurrentBindingMap()
+// Specialized forms of the concurrent map
+type BindingConcurrentMap struct {
+	ConcurrentMap
+}
 
-		// a := &boundEndpoint{NewID(), "", nil}
-		// i := NewID()
+type BindingTuple struct {
+	Key string
+	Val string
+}
 
-		// b.Set(i, a)
+// Map of type [uint64: *boundEndpint]
+func NewConcurrentBindingMap() BindingConcurrentMap {
+	return BindingConcurrentMap{NewConcurrentMap(5)}
+}
 
-		// c, _ := b.Get(i)
-		// So(c.callback, ShouldEqual, a.callback)
-	})
+// func (m BindingConcurrentMap) Iter() <-chan BindingTuple {
+// 	ch := make(chan BindingTuple, 10)
+// 	go func() {
+// 		for _, shard := range m.ConcurrentMap {
+// 			shard.RLock()
+
+// 			for key, val := range shard.items {
+// 				ch <- BindingTuple{key, val.(string)}
+// 			}
+
+// 			shard.RUnlock()
+// 		}
+
+// 		close(ch)
+// 	}()
+// 	return ch
+// }
+
+// Sets the given value under the specified key.
+func (m *BindingConcurrentMap) Set(key string, value string) {
+	m.ConcurrentMap.Set(KeyFromString(key), value)
+}
+
+// Retrieves an element from map under given key.
+func (m BindingConcurrentMap) Get(key string) (string, bool) {
+	if val, ok := m.ConcurrentMap.Get(KeyFromString(key)); ok {
+		return val.(string), true
+	} else {
+		return "", false
+	}
+}
+
+func TestGet(t *testing.T) {
+	b := NewConcurrentBindingMap()
+	b.Set("greeting", "hello")
+	c, ok := b.Get("greeting")
+
+	True(t, ok)
+	Equal(t, "hello", c)
+}
+
+func TestBadGet(t *testing.T) {
+	b := NewConcurrentBindingMap()
+	b.Set("greeting", "hello")
+	_, ok := b.Get("notagreeting")
+
+	False(t, ok)
+}
+
+func TestIterator(t *testing.T) {
+	b := NewConcurrentBindingMap()
+	b.Set("greeting", "hello")
+	_, ok := b.Get("notagreeting")
+
+	False(t, ok)
 }
